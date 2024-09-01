1393f3b5e94054f5544878d4529e1d09b61ffbfe
vitorcapdeville <vgcapdeville@hotmail.com>
2024-09-01 09:29:08 -0300

    ✨ Ajustar layout antes das imagens serem exibidas.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"; // Ativa o modo client-side para o componente
+"use client";
 
 import React, { useState } from "react";
 import axios from "axios";
@@ -11,7 +11,7 @@ export default function Home() {
   });
   const [editedImage, setEditedImage] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null); // Estado para gerenciar erros
+  const [error, setError] = useState(null);
   const [previewImages, setPreviewImages] = useState({
     image1: null,
     image2: null,
@@ -27,8 +27,8 @@ export default function Home() {
       ...prevImages,
       [name]: URL.createObjectURL(files[0]),
     }));
-    setEditedImage(null); // Limpa a imagem processada quando um novo arquivo é selecionado
-    setError(null); // Limpa o erro ao selecionar um novo arquivo
+    setEditedImage(null);
+    setError(null);
   };
 
   const handleSubmit = async (event) => {
@@ -41,7 +41,7 @@ export default function Home() {
     formData.append("box_image", selectedFiles.image2);
 
     setLoading(true);
-    setError(null); // Reseta o erro ao começar a requisição
+    setError(null);
 
     try {
       const response = await axios.post(
@@ -54,98 +54,105 @@ export default function Home() {
           responseType: "blob",
         }
       );
-      console.log("Resposta do servidor:", response);
       const imageUrl = URL.createObjectURL(response.data);
       setEditedImage(imageUrl);
     } catch (error) {
       console.error("Erro ao enviar as imagens:", error);
-      setError("Falha ao processar as imagens. Tente novamente."); // Define a mensagem de erro
+      setError("Falha ao processar as imagens. Tente novamente.");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-10xl p-8 bg-white shadow-lg rounded-lg flex">
-        <div className="w-1/5 flex flex-col space-y-3">
-          <h2 className="text-2xl font-bold mb-6 text-center">
-            Escolha um teste e um template
-          </h2>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <input
-              type="file"
-              name="image1"
-              onChange={handleFileChange}
-              accept="image/*"
-              className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-            />
-            <input
-              type="file"
-              name="image2"
-              onChange={handleFileChange}
-              accept="image/*"
-              className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-            />
-            <button
-              type="submit"
-              disabled={
-                !selectedFiles.image1 || !selectedFiles.image2 || loading
-              }
-              className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-            >
-              {loading ? "Processando..." : "Processar"}
-            </button>
-          </form>
+    <div className="min-h-screen w-full max-w-10xl p-8 bg-white shadow-lg rounded-lg flex">
+      <div className="w-1/5 flex flex-col space-y-3">
+        <h2 className="text-2xl font-bold mb-6 text-center">
+          Escolha um teste e um template
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="file"
+            name="image1"
+            onChange={handleFileChange}
+            accept="image/*"
+            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+          />
+          <input
+            type="file"
+            name="image2"
+            onChange={handleFileChange}
+            accept="image/*"
+            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+          />
+          <button
+            type="submit"
+            disabled={!selectedFiles.image1 || !selectedFiles.image2 || loading}
+            className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          >
+            {loading ? "Processando..." : "Processar"}
+          </button>
+        </form>
 
-          {error && (
-            <div className="mt-4 p-4 bg-red-100 text-red-700 rounded-lg">
-              {error}
-            </div>
-          )}
-        </div>
-
-        <div className="flex flex-col space-y-3 w-2/5 items-center">
-          {previewImages.image1 && (
-            <div>
-              <h2 className="text-lg font-semibold">Teste:</h2>
-              <Image
-                src={previewImages.image1}
-                alt="Imagem carregada 1"
-                className="rounded-lg shadow-lg"
-                width={500} // Ajuste conforme necessário
-                height={500} // Ajuste conforme necessário
-                objectFit="contain"
-              />
-            </div>
-          )}
-
-          {previewImages.image2 && (
-            <div>
-              <h2 className="text-lg font-semibold">Template:</h2>
-              <Image
-                src={previewImages.image2}
-                alt="Imagem carregada 2"
-                className="rounded-lg shadow-lg"
-                width={100} // Ajuste conforme necessário
-                height={100} // Ajuste conforme necessário
-                objectFit="contain"
-              />
-            </div>
-          )}
-        </div>
-
-        {editedImage && (
-          <div className="w-2/5 items-center">
-            <h2 className="text-lg font-semibold">Teste corrigido:</h2>
-            <Image
-              src={editedImage}
-              alt="Imagem editada"
-              className="rounded-lg shadow-lg"
-              width={500} // Ajuste conforme necessário
-              height={500} // Ajuste conforme necessário
-            />
+        {error && (
+          <div className="mt-4 p-4 bg-red-100 text-red-700 rounded-lg">
+            {error}
           </div>
+        )}
+      </div>
+
+      <div className="flex flex-col space-y-3 w-2/5 items-center">
+        <h2 className="text-lg font-semibold">Teste:</h2>
+        {!previewImages.image1 && (
+          <div className="flex h-[700px] items-center">
+            <p>Selecione um teste para exibir a visualização.</p>
+          </div>
+        )}
+        {previewImages.image1 && (
+          <Image
+            src={previewImages.image1}
+            alt="Imagem carregada 1"
+            className="rounded-lg shadow-lg"
+            height={500}
+            width={500}
+            objectFit="contain"
+          />
+        )}
+        <h2 className="text-lg font-semibold">Template:</h2>
+        {!previewImages.image2 && (
+          <div className="flex h-[100px] items-center">
+            <p>Selecione um template para exibir a visualização.</p>
+          </div>
+        )}
+        {previewImages.image2 && (
+          <Image
+            src={previewImages.image2}
+            alt="Imagem carregada 2"
+            className="rounded-lg shadow-lg"
+            width={100}
+            height={100}
+            objectFit="contain"
+          />
+        )}
+      </div>
+
+      <div className="flex flex-col space-y-3 w-2/5 items-center">
+        <h2 className="text-lg font-semibold">Teste corrigido:</h2>
+        {!editedImage && (
+          <div className="flex h-[700px] items-center">
+            <p>
+              Quando o input for processado, a imagem marcada será exibida aqui.
+            </p>
+          </div>
+        )}
+        {editedImage && (
+          <Image
+            src={editedImage}
+            alt="Imagem editada"
+            className="rounded-lg shadow-lg"
+            width={500}
+            height={500}
+          />
         )}
       </div>
     </div>
