7bf45f13a3e24f62c4f22dd976e0261f995356ce
vitorcapdeville <vgcapdeville@hotmail.com>
2024-09-01 11:25:32 -0300

    ✨ Permitir mais de uma imagem de template.

--- app/page.js
+++ app/page.js
@@ -7,27 +7,39 @@ import Image from "next/image";
 export default function Home() {
   const [selectedFiles, setSelectedFiles] = useState({
     image1: null,
-    image2: null,
+    image2: [],
   });
   const [editedImage, setEditedImage] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [previewImages, setPreviewImages] = useState({
     image1: null,
-    image2: null,
+    image2: [],
   });
-  const [threshold, setThreshold] = useState(0.5); // Novo estado para o threshold
+  const [threshold, setThreshold] = useState(0.5);
 
   const handleFileChange = (event) => {
     const { name, files } = event.target;
-    setSelectedFiles((prevFiles) => ({
-      ...prevFiles,
-      [name]: files[0],
-    }));
-    setPreviewImages((prevImages) => ({
-      ...prevImages,
-      [name]: URL.createObjectURL(files[0]),
-    }));
+    if (name === "image1") {
+      setSelectedFiles((prevFiles) => ({
+        ...prevFiles,
+        [name]: files[0],
+      }));
+      setPreviewImages((prevImages) => ({
+        ...prevImages,
+        [name]: URL.createObjectURL(files[0]),
+      }));
+    } else if (name === "image2") {
+      const fileArray = Array.from(files);
+      setSelectedFiles((prevFiles) => ({
+        ...prevFiles,
+        [name]: fileArray,
+      }));
+      setPreviewImages((prevImages) => ({
+        ...prevImages,
+        [name]: fileArray.map((file) => URL.createObjectURL(file)),
+      }));
+    }
     setEditedImage(null);
     setError(null);
   };
@@ -35,11 +47,16 @@ export default function Home() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!selectedFiles.image1 || !selectedFiles.image2) return;
+    if (!selectedFiles.image1 || selectedFiles.image2.length === 0) {
+      setError("Falha ao processar as imagens. Tente novamente.");
+      return;
+    }
 
     const formData = new FormData();
     formData.append("test_image", selectedFiles.image1);
-    formData.append("box_image", selectedFiles.image2);
+    selectedFiles.image2.forEach((file, index) => {
+      formData.append(`box_images`, file);
+    });
 
     setLoading(true);
     setError(null);
@@ -66,115 +83,129 @@ export default function Home() {
   };
 
   useEffect(() => {
-    setEditedImage(null); // Resetar a imagem editada quando o threshold for alterado
+    setEditedImage(null);
   }, [threshold]);
 
   return (
-    <div className="min-h-screen w-full max-w-10xl p-8 bg-white shadow-lg rounded-lg flex">
-      <div className="w-1/5 flex flex-col space-y-3">
-        <h2 className="text-2xl font-bold mb-6 text-center">
-          Escolha um teste e um template
-        </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <input
-            type="file"
-            name="image1"
-            onChange={handleFileChange}
-            accept="image/*"
-            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-          />
-          <input
-            type="file"
-            name="image2"
-            onChange={handleFileChange}
-            accept="image/*"
-            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-          />
-          <div className="flex flex-col space-y-2">
-            <label htmlFor="threshold" className="text-sm font-semibold">
-              Threshold: {threshold}
-            </label>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-10xl p-8 bg-white shadow-lg rounded-lg flex">
+        <div className="w-1/5 flex flex-col space-y-3">
+          <h2 className="text-2xl font-bold mb-6 text-center">
+            Escolha um teste e um template
+          </h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
             <input
-              type="range"
-              id="threshold"
-              name="threshold"
-              min="0"
-              max="1"
-              step="0.01"
-              value={threshold}
-              onChange={(e) => setThreshold(e.target.value)}
-              className="w-full"
+              type="file"
+              name="image1"
+              onChange={handleFileChange}
+              accept="image/*"
+              className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
             />
-          </div>
-          <button
-            type="submit"
-            disabled={!selectedFiles.image1 || !selectedFiles.image2 || loading}
-            className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-          >
-            {loading ? "Processando..." : "Processar"}
-          </button>
-        </form>
+            <input
+              type="file"
+              name="image2"
+              onChange={handleFileChange}
+              accept="image/*"
+              multiple
+              className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+            />
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="threshold" className="text-sm font-semibold">
+                Threshold: {threshold}
+              </label>
+              <input
+                type="range"
+                id="threshold"
+                name="threshold"
+                min="0"
+                max="1"
+                step="0.01"
+                value={threshold}
+                onChange={(e) => setThreshold(e.target.value)}
+                className="w-full"
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={
+                !selectedFiles.image1 ||
+                selectedFiles.image2.length === 0 ||
+                loading
+              }
+              className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            >
+              {loading ? "Processando..." : "Processar"}
+            </button>
+          </form>
 
-        {error && (
-          <div className="mt-4 p-4 bg-red-100 text-red-700 rounded-lg">
-            {error}
-          </div>
-        )}
-      </div>
+          {error && (
+            <div className="mt-4 p-4 bg-red-100 text-red-700 rounded-lg">
+              {error}
+            </div>
+          )}
+        </div>
 
-      <div className="flex flex-col space-y-3 w-2/5 items-center">
-        <h2 className="text-lg font-semibold">Teste:</h2>
-        {!previewImages.image1 && (
-          <div className="flex h-[700px] items-center">
-            <p>Selecione um teste para exibir a visualização.</p>
-          </div>
-        )}
-        {previewImages.image1 && (
-          <Image
-            src={previewImages.image1}
-            alt="Imagem carregada 1"
-            className="rounded-lg shadow-lg"
-            height={500}
-            width={500}
-            objectFit="contain"
-          />
-        )}
-        <h2 className="text-lg font-semibold">Template:</h2>
-        {!previewImages.image2 && (
-          <div className="flex h-[100px] items-center">
-            <p>Selecione um template para exibir a visualização.</p>
-          </div>
-        )}
-        {previewImages.image2 && (
-          <Image
-            src={previewImages.image2}
-            alt="Imagem carregada 2"
-            className="rounded-lg shadow-lg"
-            width={100}
-            height={100}
-            objectFit="contain"
-          />
-        )}
-      </div>
+        <div className="flex flex-col space-y-3 w-2/5 items-center">
+          <h2 className="text-lg font-semibold">Teste:</h2>
+          {!previewImages.image1 && (
+            <div className="flex h-[500px] items-center">
+              <p>Selecione um teste para exibir a visualização.</p>
+            </div>
+          )}
+          {previewImages.image1 && (
+            <Image
+              src={previewImages.image1}
+              alt="Imagem carregada 1"
+              className="rounded-lg shadow-lg"
+              height={0}
+              width={0}
+              style={{ width: "auto", height: "500px" }}
+            />
+          )}
+          <h2 className="text-lg font-semibold">Templates:</h2>
+          {previewImages.image2.length === 0 && (
+            <div className="flex h-[100px] items-center">
+              <p>Selecione um template para exibir a visualização.</p>
+            </div>
+          )}
+          {previewImages.image2.length > 0 && (
+            <div className="flex flex-wrap gap-2">
+              {previewImages.image2.map((src, index) => (
+                <Image
+                  key={index}
+                  src={src}
+                  alt={`Imagem carregada ${index + 2}`}
+                  className="rounded-lg shadow-lg"
+                  width={0}
+                  height={0}
+                  style={{ width: "auto", height: "100px" }}
+                />
+              ))}
+            </div>
+          )}
+        </div>
 
-      <div className="flex flex-col space-y-3 w-2/5 items-center">
-        <h2 className="text-lg font-semibold">Teste corrigido:</h2>
-        {!editedImage && (
-          <div className="flex h-[700px] items-center">
-            <p>
-              Quando o input for processado, a imagem marcada será exibida aqui.
-            </p>
-          </div>
-        )}
-        {editedImage && (
-          <Image
-            src={editedImage}
-            alt="Imagem editada"
-            className="rounded-lg shadow-lg"
-            width={500}
-            height={500}
-          />
-        )}
+        <div className="flex flex-col space-y-3 w-2/5 items-center">
+          <h2 className="text-lg font-semibold">Teste corrigido:</h2>
+          {!editedImage && (
+            <div className="flex h-[500px] items-center">
+              <p>
+                Quando o input for processado, a imagem marcada será exibida
+                aqui.
+              </p>
+            </div>
+          )}
+          {editedImage && (
+            <Image
+              src={editedImage}
+              alt="Imagem editada"
+              className="rounded-lg shadow-lg"
+              width={0}
+              height={0}
+              style={{ width: "auto", height: "500px" }}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
