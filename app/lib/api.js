87ab87731c437c19aca3c7aa1ba8facf44dcaad4
Vitor Capdeville <vgcapdeville@hotmail.com>
2024-09-19 20:45:07 -0300

    ♻️ Reativar a rota de criar imagem.

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,26 +1,16 @@
 import axios from "axios";
 
 export async function createImage(image) {
-  return {
-    _id: "66e6d9e91b5119d338adcd0c",
-    path: "uploaded_images/101fbe7f-e351-44a0-b08c-17350bbf6001_exam0_1_1.png",
-    size: {
-      width: 1652,
-      height: 2338,
-      depth: 3,
-    },
-    objects: [],
-  };
-  // const formData = new FormData();
-  // formData.append("image", image);
-  // const response = await axios.post(
-  //   `${process.env.NEXT_PUBLIC_API_URL}/save_image`,
-  //   formData,
-  //   {
-  //     headers: { "Content-Type": "multipart/form-data" },
-  //   }
-  // );
-  // return response.data;
+  const formData = new FormData();
+  formData.append("image", image);
+  const response = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/save_image`,
+    formData,
+    {
+      headers: { "Content-Type": "multipart/form-data" },
+    }
+  );
+  return response.data;
 }
 
 export async function findBoxes(image_id, box_images, threshold) {
